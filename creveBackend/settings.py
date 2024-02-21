c797fea1b41e07c360b9f62e04c896013102d4d4
MictovicDev <awaemekamichael@gmail.com>
2024-02-21 12:37:28 +0100

    creve

--- creveBackend/settings.py
+++ creveBackend/settings.py
@@ -211,6 +211,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:3000",
     "https://creve.vercel.app",
+    "http://localhost:3000",
     # Add other trusted domains as needed
 ]
 
