d43628a27277b1570a4642da130cfda105a397d0
MictovicDev <awaemekamichael@gmail.com>
2024-03-14 23:20:19 +0100

    change

--- creveBackend/asgi.py
+++ creveBackend/asgi.py
@@ -30,7 +30,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import creveBackend.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'creveBackend.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
