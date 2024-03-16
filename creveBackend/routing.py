ecea0d98c7ef376492aa0f0ddc4fe9632925c05f
MictovicDev <awaemekamichael@gmail.com>
2024-03-16 02:11:51 +0100

    done

--- creveBackend/routing.py
+++ creveBackend/routing.py
@@ -2,10 +2,11 @@
 from django.urls import path
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from core.consumers import ClientNotificationConsumer
+from core.consumers import ClientNotificationConsumer, TalentNotificationConsumer
 
 websocket_urlpatterns = [
-    path('ws/notifications/', ClientNotificationConsumer.as_asgi()),
+    path('ws/clientnotifications/', ClientNotificationConsumer.as_asgi()),
+    path('ws/talentnotifications/', TalentNotificationConsumer.as_asgi()),
 ]
 
 application = ProtocolTypeRouter({
